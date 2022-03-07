2bd14a5e6cd5dbab173e96d188fff3c013932192
Charmonson <77593573+Charmonson@users.noreply.github.com>
2022-03-07 17:17:30 -0600

    jsbasics

--- nextinLine.js
+++ nextinLine.js
@@ -113,3 +113,63 @@ function updateRecords(records, id, prop, value) {
 }
 
 updateRecords(recordCollection, 5439, 'artist', 'ABBA', 'tracks','take a chance on me');*/
+/* solution 2
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if(value=""){
+    delete[id][prop]
+  }else if (prop=="tracks"){
+    records[id][prop]= records[id][prop]||[];
+    records[id][prop].push(value);
+  }else{
+records[id][props]=value;
+  }
+  return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');*/
+
+/*// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+
+for( let i= 0; i<contacts.length; i++){
+if(contacts[i].firstName=== name){
+  if(contacts[i].hasOwnProperty(prop)){
+    return contacts[i][prop];
+    }else {
+    return "No such property";
+    }
+  }  
+}
+return "No such contact"
+
+  // Only change code above this line
+}*/
